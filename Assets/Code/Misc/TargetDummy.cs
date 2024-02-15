e573bf025d4b34d996c5de20fc57b5c31d5e9e0b
MrResistance <Paul1208Gush@Gmail.com>
2024-02-15 16:37:37 +0000

    TargetDummy refactor

--- Assets/Code/Misc/TargetDummy.cs
+++ Assets/Code/Misc/TargetDummy.cs
@@ -5,24 +5,19 @@ using UnityEngine;
 public class TargetDummy : MonoBehaviour
 {
     [SerializeField] private HingeJoint m_hingeJoint;
-    [SerializeField] private float m_downwardSpringForce;
 
     public event Action OnTargetShot;
 
     [Button]
     public void MoveTargetDown()
     {
-        JointSpring hingeSpring = m_hingeJoint.spring;
-        hingeSpring.spring = m_downwardSpringForce;
-        m_hingeJoint.spring = hingeSpring;
+        m_hingeJoint.useSpring = true;
     }
 
     [Button]
     public void MoveTargetUp()
     {
-        JointSpring hingeSpring = m_hingeJoint.spring;
-        hingeSpring.spring = 0;
-        m_hingeJoint.spring = hingeSpring;
+        m_hingeJoint.useSpring = false;
     }
 
 }
